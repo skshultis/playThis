be4a7c0c2762ca34cf6b30f51231e8d4d12e0321
General Assembly <generalassembly@Generals-Air-7.fios-router.home>
2016-08-21 18:51:42 -0400

    Adds rack cors for cross origin resource sharing.

--- playThisApi/config/application.rb
+++ playThisApi/config/application.rb
@@ -17,6 +17,26 @@ require "rails/test_unit/railtie"
 Bundler.require(*Rails.groups)
 
 module PlayThisApi
+  # ...
+
+    # Rails 3/4
+
+    config.middleware.insert_before 0, "Rack::Cors" do
+      allow do
+        origins '*'
+        resource '*', :headers => :any, :methods => [:get, :post, :options]
+      end
+    end
+
+    # Rails 5
+
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*'
+        resource '*', :headers => :any, :methods => [:get, :post, :options]
+      end
+    end
+
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
