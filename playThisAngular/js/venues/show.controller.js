ff3978a988f2e3fdf30a867d67fea072b43711ef
General Assembly <generalassembly@Generals-MacBook-Air-7.local>
2016-08-22 16:23:29 -0400

    Requests are loading in real time on show page.

--- playThisAngular/js/venues/show.controller.js
+++ playThisAngular/js/venues/show.controller.js
@@ -2,11 +2,29 @@ angular
 .module("venues")
 .controller("VenueShowController", [
   "VenueFactory",
+  "RequestFactory",
   "$stateParams",
+  "$state",
   VenueShowControllerFunction
 ]);
 
-function VenueShowControllerFunction(VenueFactory, $stateParams){
+function VenueShowControllerFunction(VenueFactory, RequestFactory, $stateParams, $state){
   var vm = this;
   vm.venue = VenueFactory.get({id: $stateParams.id});
+  //vm.comments = vm.venue.requests
+  //console.log(vm.venue);
+  vm.newComment = new RequestFactory();
+  //console.log(vm.newComment);
+  //vm.newComment.venue_id =  vm.venue.id;
+  vm.create = function(){
+      vm.newComment.venue_id = vm.venue.id;
+      vm.newComment.$save().then(function(response) {
+        console.log(response);
+        //console.log(vm.newComment)
+        vm.venue.requests.push(response);
+        vm.newComment = {};
+        // $state.go
+      });
+  };
+
 }
