c90f26cb3bf1de80f467aec238d0704743581483
General Assembly <generalassembly@Generals-MacBook-Air-7.local>
2016-08-22 10:03:33 -0400

    Front-end linked to back-end

--- playThisAngular/script.js
+++ playThisAngular/script.js
@@ -1,8 +1,29 @@
 angular
 .module("playThis", [
-  "ui.router"
+  "ui.router",
+  "ngResource"
 ])
   .config([
     "$stateProvider",
     RouterFunction
-  ]);
+  ])
+  .factory("VenueFactory", ["$resource", VenueFactoryFunc])
+  .controller("VenueController",
+[
+  "VenueFactory",
+  VenueControllerFunc
+]);
+
+function VenueControllerFunc(VenueFactory){
+  var vm = this;
+  vm.venues = VenueFactory.query()
+  console.log(vm.venues);
+}
+function VenueFactoryFunc($resource){
+  return $resource("http://localhost:3000/venues", {}, {});
+}
+
+function RouterFunction($stateProvider) {
+  //$stateProvider.state("")
+  
+}
